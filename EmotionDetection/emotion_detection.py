64adce84becaa8584437fcf70e18a049d894b576
cakirefekan <cakirefekan@gmail.com>
2025-02-19 09:10:23 -0500

    error handling implemented

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,18 +5,42 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     body =  { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = body, headers=header)
-    # Parse json
-    formatted_response = json.loads(response.text)
-    # Access the emotions in dict
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    # Initialize a dictionary to determine dominant emotion
-    dominant_emotion = {"emotion":"", "value":0}
-    # Reach each emotion to determine dominant one.
-    for emotion in (emotions):
-        # Compare the current emotion's score with assigned emotion's.
-        if float(emotions[emotion]) > float(dominant_emotion["value"]):
-            # If the current one more dominant then update 'dominant_emotion' with current values
-            dominant_emotion.update({"emotion": emotion, "value":emotions[emotion]})
-    # After checking complete, update the respond 'emotions'
-    emotions.update({'dominant_emotion' :dominant_emotion["emotion"] })
-    return emotions
+
+    if response.status_code == 200:
+        # Parse json
+        formatted_response = json.loads(response.text)
+        # Access the emotions in dict
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        # Initialize a dictionary to determine dominant emotion
+        dominant_emotion = {"emotion":"", "value":0}
+        # Reach each emotion to determine dominant one.
+        for emotion in (emotions):
+            # Compare the current emotion's score with assigned emotion's.
+            if float(emotions[emotion]) > float(dominant_emotion["value"]):
+                # If the current one more dominant then update 'dominant_emotion' with current values
+                dominant_emotion.update({"emotion": emotion, "value":emotions[emotion]})
+        # After checking complete, update the respond 'emotions'
+        emotions.update({'dominant_emotion' :dominant_emotion["emotion"] })
+        print(emotions)
+        return emotions
+    elif response.status_code == 400:
+        emotions = {
+            "sadness": None,
+            "joy" : None,
+            "anger": None,
+            "disgust": None,
+            "fear" : None,
+            "dominant_emotion": None,
+        }
+        print(emotions)
+        return emotions, 400
+    else:
+        emotions = {
+            "sadness": None,
+            "joy" : None,
+            "anger": None,
+            "disgust": None,
+            "fear" : None,
+            "dominant_emotion": None,
+        }
+        return emotions, response.status_code
--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, redirect, request
 from EmotionDetection.emotion_detection import emotion_detector
-
-
 app = Flask(__name__)
 
 @app.route("/emotionDetector", methods=["GET", "POST"])
@@ -9,14 +7,33 @@ def detect_emotion():
     statement = request.args.get("textToAnalyze")
     if statement :
         emotions = emotion_detector(statement)
-        return f"""For the given statement, the system response is 'anger': {emotions['anger']},
-        'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and
-        'sadness': {emotions['sadness']}. The dominant emotion is {emotions['dominant_emotion']}."""
+        print("aa\n")
+        print(request)
+        print("aa\n")
+
+        if emotions["dominant_emotion"] is None:
+            return "Invalid text! Please try again!"
+        else:
+            return f"""For the given statement, the system response is 'anger': {emotions['anger']},
+            'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and
+            'sadness': {emotions['sadness']}. The dominant emotion is {emotions['dominant_emotion']}."""
+    else:
+        emotions = emotion_detector(statement)
+        print("aa\n")
+        print(request)
+        print("aa\n")
+
+        if emotions[0]["dominant_emotion"] is None:
+            return "Invalid text! Please try again!"
+        else:
+            return f"""For the given statement, the system response is 'anger': {emotions['anger']},
+            'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and
+            'sadness': {emotions['sadness']}. The dominant emotion is {emotions['dominant_emotion']}."""
     return {'error':404,'message':'You have to express a statement'}, 404
 
 @app.route("/")
 def open_index():
     return render_template("index.html")
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
