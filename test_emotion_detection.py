760d256c2bb36e0d28b2a120b35b3a5c38fcd2ef
cakirefekan <cakirefekan@gmail.com>
2025-02-19 06:01:15 -0500

    unit tests are completed

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -4,6 +4,22 @@ class TestEmotionDetection(unittest.TestCase):
     def test_emotion_detector(self):
         inp = "I am glad this happened"
         expected = "joy"
-        return self.assertEqual(emotion_detector(inp)['dominant_emotion'], expected)
+        self.assertEqual(emotion_detector(inp)['dominant_emotion'], expected)
+
+        inp = "I am really mad about this"
+        expected = "anger"
+        self.assertEqual(emotion_detector(inp)['dominant_emotion'], expected)
+
+        inp = "I feel disgusted just hearing about this"
+        expected = "disgust"
+        self.assertEqual(emotion_detector(inp)['dominant_emotion'], expected)
+
+        inp = "I am so sad about this"
+        expected = "sadness"
+        self.assertEqual(emotion_detector(inp)['dominant_emotion'], expected)
+
+        inp = "I am really afraid that this will happen"
+        expected = "fears"
+        self.assertEqual(emotion_detector(inp)['dominant_emotion'], expected)
 
 unittest.main()
