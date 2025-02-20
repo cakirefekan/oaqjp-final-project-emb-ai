452de8a975a6b08cb22b27d957b220d9173a2c0b
cakirefekan <cakirefekan@gmail.com>
2025-02-20 04:33:25 -0500

    Improved static code analysis score

--- server.py
+++ server.py
@@ -1,38 +1,29 @@
-from flask import Flask, render_template, redirect, request
+""" server """
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 
 @app.route("/emotionDetector", methods=["GET", "POST"])
 def detect_emotion():
+    """ Function handles /emotionDetector routes."""
     statement = request.args.get("textToAnalyze")
     if statement :
         emotions = emotion_detector(statement)
-        print("aa\n")
-        print(request)
-        print("aa\n")
-
         if emotions["dominant_emotion"] is None:
             return "Invalid text! Please try again!"
-        else:
-            return f"""For the given statement, the system response is 'anger': {emotions['anger']},
-            'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and
-            'sadness': {emotions['sadness']}. The dominant emotion is {emotions['dominant_emotion']}."""
-    else:
-        emotions = emotion_detector(statement)
-        print("aa\n")
-        print(request)
-        print("aa\n")
-
-        if emotions[0]["dominant_emotion"] is None:
-            return "Invalid text! Please try again!"
-        else:
-            return f"""For the given statement, the system response is 'anger': {emotions['anger']},
-            'disgust': {emotions['disgust']}, 'fear': {emotions['fear']}, 'joy': {emotions['joy']} and
-            'sadness': {emotions['sadness']}. The dominant emotion is {emotions['dominant_emotion']}."""
+        return f"""For the given statement, the system response is 'anger':
+            {emotions['anger']}, 'disgust': {emotions['disgust']}, 'fear':
+            {emotions['fear']}, 'joy': {emotions['joy']} and 'sadness':
+            {emotions['sadness']}. The dominant emotion is
+            {emotions['dominant_emotion']}."""
+    emotions = emotion_detector(statement)
+    if emotions[0]["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
     return {'error':404,'message':'You have to express a statement'}, 404
 
 @app.route("/")
 def open_index():
+    """ Function handles / routes."""
     return render_template("index.html")
 
 if __name__ == "__main__":
